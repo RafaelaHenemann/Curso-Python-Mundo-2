a46491b509ba382e96915756dfe13c20bb42e4d5
Rafaela Henemann <rafaitaly@gmail.com>
2021-09-03 17:11:17 -0300

    Update

--- Desafios/ex042_analisando triangulos v2.0.py
+++ Desafios/ex042_analisando triangulos v2.0.py
@@ -1,1 +1,12 @@
 # Refaça o desafio 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: EQUILÁTERO - Todos os lados iguais; ISÓSCELES - Dois lados iguais, um diferente; ESCALENO - Todos os lados diferentes.
+
+print('-='*20)
+print('Analisador de Triângulos')
+print('-='*20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo')
--- Desafios/ex044_gerenciador de pagamentos.py
+++ Desafios/ex044_gerenciador de pagamentos.py
@@ -1,1 +1,20 @@
 # Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento: à vista dinheiro/cheque - 10% de desconto; à vista no cartão - 5% de desconto; em até 2x no cartão - preço normal; 3x ou mais no cartão - 20% de juros.
+
+p = float(input('Qual o valor do produto que você está comprando? R$'))
+print('Formas de Pagamento:\n'
+'[1] A vista no dinheiro/cheque - 10% de desconto\n'
+'[2] A vista no cartão - 5% de desconto\n'
+'[3] Em até 2x no cartão de crédito - Preço normal do produto\n'
+'[4] Em 3x ou mais no cartão de crédito - 20% de juros')
+f = int(input('Qual opção de pagamento acima você gostaria de escolher (indique o número)? '))
+if f == 1:
+    print('Você escolheu pagar a vista no dinheiro/cheque, seu produto no valor de R${:.2f}, terá 10% de desconto e ficará por R${:.2f}.'.format(p, p - (p * 10 / 100)))
+elif f == 2:
+    print('Você escolheu pagar a vista no cartão, seu produto no valor de R${:.2f}, terá 5% de desconto e ficará por R${:.2f}.'.format(p, p - (p * 5 / 100)))
+elif f == 3:
+    print('Você escolheu pagar em 2x no cartão, seu produto no valor de R${:.2f}, manterá seu valor e suas parcelas saírão por R${:.2f} cada.'.format(p, p / 2))
+elif f == 4:
+    pa = int(input('Em quantas parcelas gostaria de fazer? '))
+    print('Você escolheu parcelar o seu produto em {} vezes, haverá um acréscimo de 20% no valor, portanto seu produto que era R${:.2f} passará a custar R${:.2f}, e cada uma das parcelas sairá por R${:.2f}.'.format(pa, p, p + (p * 20 / 100), (p + (p * 20 / 100)) / pa))
+else:
+    print('Opção inválida, reinicie o processo.')
