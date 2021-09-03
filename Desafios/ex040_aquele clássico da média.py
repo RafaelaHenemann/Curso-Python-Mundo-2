253cc4d65b293d8bb8d01332f011e673e2e35058
Rafaela Henemann <rafaitaly@gmail.com>
2021-09-03 13:56:09 -0300

    Update

--- Desafios/ex040_aquele clássico da média.py
+++ Desafios/ex040_aquele clássico da média.py
@@ -1,1 +1,11 @@
 # Crie um programa que leia duas notas de um aluno e calcule a média, mostrando uma mensagem no final, de acordo com a média atingida: Média abaixo de 5.0 - REPROVADO; Média entre 5.0 e 6.9 - RECUPERAÇÃO; Média acima de 7.0 - APROVADO.
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+if m < 5.0:
+    print('Sua média foi {:.1f}, infelizmente você foi reprovado!'.format(m))
+elif m >= 5.0 and m <= 6.9:
+    print('Sua média foi {:.1f}, você está de recuperação.'.format(m))
+else:
+    print('Sua média foi {:.1f}, parabéns pela aprovação!'.format(m))
+    
--- Desafios/ex041_classificando atletas.py
+++ Desafios/ex041_classificando atletas.py
@@ -1,1 +1,17 @@
 # A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre sua categoria, de acordo com a idade: Até 9 anos - MIRIM; Até 14 anos - INFANTIL; Até 19 anos - JÚNIOR; Até 25 anos - SÊNIOR; Acima de 25 anos - MASTER.
+
+from datetime import date, datetime
+n = int(input('Em que ano o atleta nasceu (quatro dígitos)? '))
+a = date.today().year
+i = a - n
+if i <= 9:
+    print('O atleta tem {} anos, portanto está na categoria MIRIM.'.format(i))
+elif i > 9 and i <= 14:
+    print('O atleta tem {} anos, portanto está na categoria INFANTIL.'.format(i))
+elif i > 14 and i <= 19:
+    print('O atleta tem {} anos, portanto está na categoria JÚNIOR.'.format(i))
+elif i > 19 and i <= 25:
+    print('O atleta tem {} anos, portanto está na categoria SÊNIOR.'.format(i))
+else:
+    print('O atleta tem {} anos, portanto está na categoria MASTER.'.format(i))
+    
