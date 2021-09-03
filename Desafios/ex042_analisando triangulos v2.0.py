aa42ee06c28c8ec782650fdcdb0bddeaf86d47c6
Rafaela Henemann <rafaitaly@gmail.com>
2021-09-03 17:24:19 -0300

    Update

--- Desafios/ex042_analisando triangulos v2.0.py
+++ Desafios/ex042_analisando triangulos v2.0.py
@@ -8,5 +8,11 @@ r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima PODEM FORMAR triângulo!')
+    if r1 == r2 != r3 or r1 == r3 != r2 or r2 == r3 != r1:
+        print('O triângulo formado é do tipo ISÓSCELES.')
+    elif r1 == r2 and r2 == r3:
+        print('O triângulo formado é do tipo EQUILÁTERO.')
+    else:
+        print('O triângulo formado é do tipo ESCALENO.')
 else:
     print('Os segmentos acima NÃO PODEM FORMAR triângulo')
